c4944772cd5fe81f1c7ad5ae7c4049997e07d46c
Ollie Snowden <oliversnowden@snodnipper.co.uk>
2018-08-29 11:32:09 +0100

    Update run-index.sh

--- run-index.sh
+++ run-index.sh
@@ -3,6 +3,7 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
+    echo "e.g. gb-buildings.gpkg"
 fi
 
 docker run -v $(pwd):/data gd:latest ogrinfo $1 -sql "SELECT CreateSpatialIndex('multipolygons', 'geom')"
